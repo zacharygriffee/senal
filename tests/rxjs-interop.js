20c9730f1cf68b581450619cf84dec8d9250d43b
Zachary Griffee <zacharygriffee@gmail.com>
2024-03-01 18:18:59 -0500

    - browser compatibility    - handle cases where an object property cannot be made reactive no matter what we do     e.g. some window properties, crypto, etc.     - browser test (tests/test.html)

--- tests/rxjs-interop.js
+++ tests/rxjs-interop.js
@@ -1,9 +1,9 @@
-import {test} from "brittle";
-import * as rxjs from "rxjs";
-import * as rxjsOperators from "rxjs/operators";
-
-const rx = {...rxjsOperators, rxjs};
-
-test("A rxjs op can work", async t => {
-
-});
+// import {test} from "brittle";
+// import * as rxjs from "rxjs";
+// import * as rxjsOperators from "rxjs/operators";
+//
+// const rx = {...rxjsOperators, rxjs};
+//
+// test("A rxjs op can work", async t => {
+//
+// });
