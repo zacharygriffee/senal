5980bb1a5c0fc7192907603e697216c7c198af0e
Zachary Griffee <zacharygriffee@gmail.com>
2024-03-21 23:15:33 -0400

    - setInciterSymbolMaker for custom inciter deterministic symbol generation

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ export { pause } from "./lib/pause.js";
 export { ignore } from './lib/ignore.js';
 export { tada } from './lib/tada.js';
 export { senal} from './lib/senal.js';
-export { inciter } from "./lib/inciter.js";
+export { inciter, setInciterSymbolMaker } from "./lib/inciter.js";
